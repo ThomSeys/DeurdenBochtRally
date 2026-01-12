a3c6f7aabfacb73d683491b893a63744b8c79fe5
ThomasSeyssensTPO <thomas.seyssens@two-point-o.be>
2026-01-12 22:36:09 +0100

    fix: properly import dependencies in vercel function

--- apps/web/api/index.js
+++ apps/web/api/index.js
@@ -1,1 +1,4 @@
-export { default } from "../build/server/index.js";
+import { createRequestHandler } from "@react-router/node";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({ build });
