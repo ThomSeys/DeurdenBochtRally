8516d4146f0e7be0e98d824c78d41787f941feba
ThomasSeyssensTPO <thomas.seyssens@two-point-o.be>
2026-01-12 22:44:58 +0100

    fix: use dynamic import and proper handler pattern

--- apps/web/api/index.js
+++ apps/web/api/index.js
@@ -1,4 +1,7 @@
 import { createRequestHandler } from "@react-router/node";
-import * as build from "./build/server/index.js";
 
-export default createRequestHandler({ build });
+export default async function handler(req, res) {
+  const build = await import("../build/server/index.js");
+  const requestHandler = createRequestHandler({ build });
+  return requestHandler(req, res);
+}
