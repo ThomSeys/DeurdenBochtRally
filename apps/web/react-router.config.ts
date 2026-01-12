09b68e7baae32fad97b2aedf5eecc03b20cee897
ThomasSeyssensTPO <thomas.seyssens@two-point-o.be>
2026-01-12 22:15:31 +0100

    trying cleanup

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -2,9 +2,4 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  buildEnd: async ({ buildManifest }) => {
-    await import("@react-router/architect").then((mod) =>
-      mod.unstable_vercel({ buildManifest })
-    );
-  },
 } satisfies Config;
